91e158cbadb430c6969864a9cf78cecced30c17a
BrianJMagee <brianmagee300@gmail.com>
2025-07-10 15:19:03 +0100

    created weather app files
    I will create a weather app using tkinter and api calling

---
+++ python/api_learning/weather.py
@@ -0,0 +1,19 @@
+class weather:
+    def __init__(self):
+        pass
+
+    def get_weather():
+        #get weather data
+        #match the status code in a match case statement which will then send the error code to be displayed via display_error
+        pass
+
+
+
+    #I SHOULD PROB MOVE THE DISPLAY STUFF TO THE GUI APP, AND KEEP ALL THE CONVERSION STUFF HERE, IN A DIFFERENT FUNCTION
+    def display_weather():
+        #does all conversions then display
+        pass
+
+    def display_error():
+        pass
+
